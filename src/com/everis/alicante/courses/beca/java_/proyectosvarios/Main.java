8b02eb1de7f8cf94046a871a90485a5c50e81335
Cristyna <cristyna1989@hotmail.com>
2017-07-14 20:05:09 +0200

    cambios en la clase

--- src/com/everis/alicante/courses/beca/java_/proyectosvarios/Main.java
+++ src/com/everis/alicante/courses/beca/java_/proyectosvarios/Main.java
@@ -18,7 +18,26 @@ public class Main {
 		String str3= str+str2;
 		str3=str.concat(str2).concat(str);
 		System.out.println(str3);
-
+		
+		int[] arrayEnteros = new int [10];//Creamos el array
+		
+		arrayEnteros[0]=0;
+		arrayEnteros[1]=1;
+		arrayEnteros[2]=2;
+		arrayEnteros[3]=3;
+		arrayEnteros[4]=4;
+		arrayEnteros[5]=5;
+		arrayEnteros[6]=6;
+		arrayEnteros[7]=7;
+		arrayEnteros[8]=8;
+		arrayEnteros[9]=9;
+		//arrayEnteros[10]=10;//esta posicion no existe te has pasado las dimensiones
+		
+		Cliente[] clientes = new Cliente[3];
+		clientes [0] = cliente1;
+		clientes [1] = cliente2;
+		clientes [2] = cliente3;
+		
 	}
 
 }
