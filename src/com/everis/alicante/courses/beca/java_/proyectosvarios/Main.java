6b93439b50822ea61d70275e857810d0c1a5c398
Cristyna <cristyna1989@hotmail.com>
2017-07-14 18:56:03 +0200

    Main del proyecto

---
+++ src/com/everis/alicante/courses/beca/java_/proyectosvarios/Main.java
@@ -0,0 +1,24 @@
+package com.everis.alicante.courses.beca.java_.proyectosvarios;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Tipos de datos primitivos
+		
+		boolean result =true;
+		char letraC = 'C';
+		byte b = 100;
+		short s =1000;
+		double f = 10.65;
+		float f1 = (float) 10.6;
+		
+		System.out.println(f);
+		String str= new String ("test");
+		String str2= "hola";
+		String str3= str+str2;
+		str3=str.concat(str2).concat(str);
+		System.out.println(str3);
+
+	}
+
+}
